6a7f4f1b586142c6bb064a66540675b6b35c3132
BAEEUNMANGDUCK <122082279+BAEEUNMANGDUCK@users.noreply.github.com>
2023-10-30 12:09:43 +0900

    Update urls.py

--- django_plotly/core/urls.py
+++ django_plotly/core/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.chart, name='chart'),
-    path("index/", views.use_template, name="usetemplate"),
+    path('index/', views.chart, name='chart'),
+    path("", views.use_template, name="usetemplate"),
     path("update_chart/", views.update_chart, name='update_chart')
 ] 
